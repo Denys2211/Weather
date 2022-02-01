34fe4ed366da6ebfc2176cc98df0a548ee56d394
mac <tsurkanovskyi@gmail.com>
2022-02-01 22:11:14 +0200

    added TemperatureConverter

---
+++ Weather/Converters/TemperatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Weather.Converters
+{
+    public class TemperatureConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string temperature = ((float)value).ToString();
+            var splittemperature = temperature.Split('.');
+
+            return splittemperature[0]+ "°";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
